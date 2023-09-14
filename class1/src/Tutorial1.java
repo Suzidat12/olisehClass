2dd3995c60c22255e5f355e78db3aba39507361f
Sunkanmi <sunkanmi@credocentral.com>
2023-09-14 21:42:34 +0100

    First push

---
+++ class1/src/Tutorial1.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+
+public class Tutorial1 {
+    // Data types: String- reference data types, (Long, Int, Double,Float, Char, Boolean, BigDecimal)primitive data types ......
+    // 2.57
+
+    int x;// declaration
+    int y = 4; // Initializing
+    String result = "Good result";
+    int st = 4;
+    double sy = 4.5;
+    float zy = 2.5F;
+    Character io = 'M';
+
+    public static void main(String[] args) {
+        Tutorial1 tutorial1 = new Tutorial1(); // declaration of class...
+        System.out.println(tutorial1.result);
+        System.out.println(tutorial1.x);
+        System.out.println(tutorial1.zy);
+        System.out.println(tutorial1.io);
+        System.out.println(tutorial1.sy);
+    }
+
+}
+
