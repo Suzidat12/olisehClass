8dc1d6cbc1443ba6c51f4f1458c1e7867f2229b5
Sunkanmi <sunkanmi@credocentral.com>
2023-10-05 21:33:17 +0100

    Latest class

--- class1/src/Assignment.java
+++ class1/src/Assignment.java
@@ -19,25 +19,48 @@ public class Assignment {
 //            System.out.println("F");
 //        }
 
-       Integer day = 2;
-        switch (day){
-            case 1:
-                System.out.println("Monday");
+//       Integer day = 2;
+//        switch (day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            default:
+//                System.out.println("No weeks exist");
+//        }
+        //For loop,for-each loop, while -loop and do-while, break and continue (1,2,3,4,5)
+        int y = 20;
+
+//        for(int i=1;i<y;++i){
+//            System.out.println(i);
+//        }
+//        while (i<y){
+//            System.out.println(i);
+//            i++;
+//        }
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<y);
+        for(int i=1;i<y;i++){
+            if(i==5){
                 break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            default:
-                System.out.println("No weeks exist");
+            }
+//            if(i>4 && i<10){
+//                continue;
+//            }
+            System.out.println(i);
         }
 
     }
