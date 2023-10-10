965c10c0d2a71a80b5cd9afd87826c8782f1be6d
Sunkanmi <sunkanmi@credocentral.com>
2023-10-10 21:03:21 +0100

    the name is assignment1

---
+++ class1/src/Assignment1.java
@@ -0,0 +1,19 @@
+package com.zik.ussd_application.accountRepo;
+
+import java.util.Arrays;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+       //[]-> it should not exist the number that you put in the array
+        int[] names = new int[5];
+        names[0] = 1;
+        names[1] = 2;
+        names[2]=3;
+        names[3]=4;
+        names[4]=5;
+
+      //  System.out.println(Arrays.toString(names));
+        int[] score = {1,2,3,4,5};
+        System.out.println(Arrays.toString(score));
+    }
+}
